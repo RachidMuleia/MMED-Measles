41a037bbcc8016c046100c6533edd8744058a61c
BoazBaliejukia <baliejukiabaliesima@gmail.com>
2026-06-22 21:16:58 +0200

    Delete Descriptive.R

--- Descriptive analysis/Descriptive.R
+++
@@ -1,9 +0,0 @@
-# MMED project : Measles Transmission Dynamics DRC 2019
-
-## Loading packages
-
-library(tidyverse)
-
-## Read in data
-
-cases <- read_csv("/Users/boazbaliejukia/Documents/PMF_Project/Measles_data_cleaning/MMEDGit/cases.csv")
